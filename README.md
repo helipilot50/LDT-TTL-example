280d4c4dfd87def000e39e80841bca54b2b0fd65
Peter Milne <helipilot50@gmail.com>
2015-08-19 12:07:16 -0500

    initial project creation

---
+++ README.md
@@ -0,0 +1,12 @@
+#ldt-ttl-example
+
+##Problem
+
+##Solution
+yada yada yada
+
+The source code for this solution is available on GitHub, and the README.md 
+http://github.com/some place. 
+
+
+##Discussion
---
+++ src/test/java/com/aerospike/examples/ldt/ttl/LdtExpireTest.java
@@ -0,0 +1,48 @@
+/* 
+ * Copyright 2012-2015 Aerospike, Inc.
+ *
+ * Portions may be licensed to Aerospike, Inc. under one or more contributor
+ * license agreements.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.aerospike.examples.ldt.ttl;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.aerospike.client.AerospikeClient;
+
+public class LdtExpireTest {
+
+	AerospikeClient client;
+	LdtExpire subject;
+
+	@Before
+	public void setUp() throws Exception {
+		client = new AerospikeClient("localhost", 3000);
+		subject = new LdtExpire(client, "test", "demo");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		client.close();
+	}
+
+	@Test
+	public void testLdtExpire() throws Exception {
+		subject.work();
+	}
+
+}
